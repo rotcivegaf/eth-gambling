e66b703b8804743594dec93dc14e1cbaf6f4bb23
rotcivegaf <victorfage@gmail.com>
2018-12-02 22:49:19 -0300

    change port coverage

--- truffle.js
+++ truffle.js
@@ -9,7 +9,7 @@ module.exports = {
         coverage: {
             host: 'localhost',
             network_id: '*', // eslint-disable-line camelcase
-            port: 8545,
+            port: 8555,
             gas: 0xfffffffffff,
             gasPrice: 0x01,
         },
