0f4ef4ccb44a961a205902c05febb64b3b7d5910
rotcivegaf <victorfage@gmail.com>
2018-12-03 09:16:11 -0300

    work in P2P model

--- contracts/model/P2P.sol
+++ contracts/model/P2P.sol
@@ -4,22 +4,37 @@ import "../interfaces/IModel.sol";
 
 import "../utils/BytesUtils.sol";
 
-contract DecodeData is BytesUtils{
-    uint256 public constant L_CREATE_DATA = 32 + 32 + 16 + 16;  // player A option + player B option + player A pay + player B pay
 
-    function _decodeCreateData(
+contract DecodeData is BytesUtils {
+    // player B option + player A pay + player B pay
+    uint256 public constant L_CREATE_PLAY_DATA = 32 + 16 + 16;
+
+    /**
+        @notice Decode bytes array and returns the parameters of create and play a bet
+
+        @dev The length of data should be L_CREATE_PLAY_DATA
+        @param _data Index of the loan
+            from-to bytes
+            0 -32: playerBOption
+            32-48: playerAPay
+            48-64: playerBPay
+
+        @return playerBOption The option that the second player should play
+        @return playerAPay The amount of the player A will pay to player B if he loses
+        @return playerBPay The amount of the player B will pay to player A if he loses
+    */
+    function _decodeCreatePlayData(
         bytes _data
-    ) internal pure returns (bytes32, bytes32, uint128, uint128) {
-        require(_data.length == L_CREATE_DATA, "Invalid create data length");
-        (bytes32 playerAOption, bytes32 playerBOption, bytes32 playerAPay, bytes32 playerBPay) = decode(
+    ) internal pure returns (bytes32, uint128, uint128) {
+        require(_data.length == L_CREATE_PLAY_DATA, "Invalid create data length");
+        (bytes32 playerBOption, bytes32 playerAPay, bytes32 playerBPay) = decode(
             _data,
-            32,
             32,
             16,
             16
         );
 
-        return (playerAOption, playerBOption, uint128(playerAPay), uint128(playerBPay));
+        return (playerBOption, uint128(playerAPay), uint128(playerBPay));
     }
 }
 
@@ -30,12 +45,11 @@ contract P2P is IModel, DecodeData {
         address playerB;
         bytes32 playerAOption;
         bytes32 playerBOption;
-        uint128 playerAPay;
-        uint128 playerBPay;
+        uint128 playerAPay; // Pay A to B
+        uint128 playerBPay; // Pay B to A
     }
 
     mapping(bytes32 => Bet) public bets;
-    mapping(bytes32 => mapping(address => uint128)) public restPay;
 
     address public gamblingManager;
 
@@ -55,116 +69,112 @@ contract P2P is IModel, DecodeData {
     ) external
         onlyGamblingManager
     {
-        (
-            bytes32 playerAOption,
-            bytes32 playerBOption,
-            uint128 playerAPay,
-            uint128  playerBPay
-        ) = _decodeCreateData(_data);
-
-        require(playerAPay > 0 && playerBPay > 0, "The pay amounts should not be 0");
-        require(
-            playerAOption != 0x0 && playerBOption != 0x0 && playerAOption != playerBOption,
-            "The players do not have an option or are equal"
-        );
-
-        bets[_id] = Bet({
-            playerA: 0x0,
-            playerB: 0x0,
-            playerAOption: playerAOption,
-            playerBOption: playerBOption,
-            playerAPay: playerAPay, // Pay A to B
-            playerBPay: playerBPay // Pay B to A
-        });
+        revert("Not implements");
     }
 
     function playBet(
         bytes32 _betId,
         address _player,
-        bytes32 _option
+        bytes32
     ) external
         onlyGamblingManager
-    returns(uint256 needAmount){
+    returns (uint256 needAmount) {
         Bet storage bet = bets[_betId];
-        require(bet.playerAOption == 0x0, "The bet its taken");
+        require(bet.playerB == 0x0, "The bet its taken");
 
-        if (_option == bet.playerAOption)
-            bet.playerA = _player;
-        else
-            bet.playerB = _player;
+        bets[_betId].playerB = _player;
 
-        return bet.playerAPay > bet.playerBPay ? bet.playerAPay : bet.playerBPay;
+        needAmount = bet.playerAPay > bet.playerBPay ? bet.playerAPay : bet.playerBPay;
     }
 
+    /**
+        @dev The msg.sender should be the gamblingManager,
+            anyone can collect a bet without be the player A or B.
+
+        @param _player Who create and play the bet as player A
+        @param _option The option of player A
+        @param _data Look in _decodeCreatePlayData documentation for more information
+
+        @return The amount needed to place the bet
+    */
     function createPlayBet(
-        bytes32,
-        address,
-        bytes32,
-        bytes
+        bytes32 _id,
+        address _player,
+        bytes32 _option,
+        bytes _data
     ) external
         onlyGamblingManager
-    returns(uint256){
-        return 0;
+    returns(uint256 needAmount) {
+        (
+        bytes32 playerBOption,
+        uint128 playerAPay,
+        uint128  playerBPay
+        ) = _decodeCreatePlayData(_data);
+
+        require(playerAPay > 0 && playerBPay > 0, "The pay amounts should not be 0");
+        require( _option != playerBOption, "The options should not be equal");
+
+        bets[_id] = Bet({
+            playerA: _player,
+            playerB: 0x0,
+            playerAOption: _option,
+            playerBOption: playerBOption,
+            playerAPay: playerAPay,
+            playerBPay: playerBPay
+        });
+
+        needAmount = playerAPay > playerBPay ? playerAPay : playerBPay;
     }
 
+    /**
+        @dev The msg.sender should be the gamblingManager,
+            anyone can collect a bet without be the player A or B.
+
+        @param _player Who play the bet, can be the player A or B.
+        @param _winner Must be returned by the oracle of the bet,
+            it may be option A or B and if not, the bet is considered a draw.
+
+        @return The amount that will be transferred to the _player
+    */
     function collectBet(
         bytes32 _betId,
         address _player,
         bytes32 _winner
     ) external
         onlyGamblingManager
-    returns(uint256 amount){
+    returns(uint256 amount) {
         Bet storage bet = bets[_betId];
-        require(bet.playerA != 0x0 || bet.playerB != 0x0, "The bet its not taken");
-        uint256 senderRestPay = restPay[_betId][_player];
+        require(bet.playerB != 0x0, "The bet its not taken");
 
-        if (senderRestPay > 0){ // to pay in a draw
-            amount = senderRestPay;
-            restPay[_betId][_player] = 0;
-            return;
-        }
 
-        if (_winner == bet.playerAOption && _player == bet.playerA){
+        require(bet.playerA == _player || bet.playerB == _player, "");
+
+        if (_winner == bet.playerAOption && _player == bet.playerA) {
             amount = bet.playerAPay + bet.playerBPay;
-            bet.playerAPay = 0;
-            bet.playerBPay = 0;
         } else {
-            if (_winner == bet.playerBOption && _player == bet.playerB){
+            if (_winner == bet.playerBOption && _player == bet.playerB) {
                 amount = bet.playerAPay + bet.playerBPay;
-                bet.playerAPay = 0;
-                bet.playerBPay = 0;
             } else {
-                if (_player == bet.playerA){
-                    restPay[_betId][bet.playerB] = bet.playerAPay;
+                if (_player == bet.playerA) {
                     amount = bet.playerBPay;
-                    bet.playerAPay = 0;
-                    bet.playerBPay = 0;
                 } else {
                     if (_player == bet.playerB) {
-                        restPay[_betId][bet.playerA] = bet.playerBPay;
                         amount = bet.playerAPay;
-                        bet.playerAPay = 0;
-                        bet.playerBPay = 0;
                     }
                 }
             }
         }
+
+        bet.playerAPay = 0;
+        bet.playerBPay = 0;
     }
 
-    function cancelBet( // TODO FIX
+    function cancelBet(
         bytes32 _betId,
         address _player
     ) external
         onlyGamblingManager
     {
-        Bet storage bet = bets[_betId];
-
-        if (_player == bet.playerA && bet.playerB == 0x0) {
-            delete bets[_betId];
-        }
-
-        if (_player == bet.playerB && bet.playerA == 0x0) {
-            delete bets[_betId];
-        }
+        require(bets[_betId].playerB == 0x0, "The bet its taken");
     }
 }
