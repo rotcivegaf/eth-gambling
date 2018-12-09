9a97338bb2fdd82816518a5edea226ec58be13e8
rotcivegaf <victorfage@gmail.com>
2018-12-09 19:33:29 -0300

    remove validateCreatePlay, indent functions

--- contracts/interfaces/IOracle.sol
+++ contracts/interfaces/IOracle.sol
@@ -2,8 +2,18 @@ pragma solidity ^0.4.24;
 
 
 contract IOracle {
-    function validateCreate(bytes32 _eventId, bytes _data) external view returns(bool);
-    function validatePlay(bytes32 _eventId, bytes32 _option, bytes _data) external view returns(bool);
-    function validateCreatePlay(bytes32 _eventId, bytes32 _option, bytes _data) external view returns(bool);
-    function whoWon(bytes32 _eventId) external view returns(bytes32 winOption);
+    function validateCreate(
+        bytes32 _eventId,
+        bytes _data
+    ) external view returns(bool);
+
+    function validatePlay(
+        bytes32 _eventId,
+        bytes32 _option,
+        bytes _data
+    ) external view returns(bool);
+
+    function whoWon(
+        bytes32 _eventId
+    ) external view returns(bytes32 winOption);
 }
