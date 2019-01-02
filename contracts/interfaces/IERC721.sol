50d310c9b582f1398e44c8146008ea878e760dbb
rotcivegaf <victorfage@gmail.com>
2019-01-01 22:40:31 -0300

    refactor

--- contracts/interfaces/IERC721.sol
+++ contracts/interfaces/IERC721.sol
@@ -8,11 +8,12 @@ interface IERC721 {
 
     function balanceOf(address _owner) external view returns (uint256);
     function ownerOf(uint256 _tokenId) external view returns (address);
+    function getApproved(uint256 _tokenId) external view returns (address);
+    function isApprovedForAll(address _owner, address _operator) external view returns (bool);
+
     function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes calldata data) external;
     function safeTransferFrom(address _from, address _to, uint256 _tokenId) external;
     function transferFrom(address _from, address _to, uint256 _tokenId) external;
     function approve(address _approved, uint256 _tokenId) external;
     function setApprovalForAll(address _operator, bool _approved) external;
-    function getApproved(uint256 _tokenId) external view returns (address);
-    function isApprovedForAll(address _owner, address _operator) external view returns (bool);
 }
