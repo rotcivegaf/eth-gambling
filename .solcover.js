80fe9b4eba52788177a9348ad06c26eb7476f224
rotcivegaf <victorfage@gmail.com>
2019-01-29 03:10:50 -0300

    add interfaces to skipFiles

--- .solcover.js
+++ .solcover.js
@@ -2,7 +2,8 @@ module.exports = {
     norpc: true,
     testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
     compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
-    skipFiles: [ // TODO: Remove skip BytesUtils when solidity-coverage gets patched
-        'utils/test'
+    skipFiles: [
+        'utils/test',
+        'interfaces'
     ]
 }
