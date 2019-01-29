dae43e100fe0323afb83281da3b0874076c822fb
rotcivegaf <victorfage@gmail.com>
2019-01-28 23:27:43 -0300

    refactor

--- contracts/interfaces/IGamblingManager.sol
+++ contracts/interfaces/IGamblingManager.sol
@@ -2,10 +2,50 @@ pragma solidity ^0.5.0;
 
 
 interface IGamblingManager {
-    event Created(address indexed _creator, bytes32 indexed _id, address _token, bytes32[] _data, uint256 _nonce);
-    event Created2(address indexed _creator, bytes32 indexed _id, address _token, bytes32[] _data, uint256 _salt);
-    event Created3(address indexed _creator, bytes32 indexed _id, address _token, bytes32[] _data, uint256 _salt);
-    event Played(bytes32 indexed _id, uint256 _tip, uint256 _amount, bytes32[] _data);
-    event Collected(address indexed _collecter, address indexed _beneficiary, bytes32 indexed _id, uint256 _tip, uint256 _amount, bytes32[] _data);
-    event Canceled(address indexed _creator, bytes32 indexed _id, uint256 _amount, bytes32[] _data);
+    event Created(
+        address indexed _creator,
+        bytes32 indexed _id,
+        address _token,
+        bytes32[] _data,
+        uint256 _nonce
+    );
+
+    event Created2(
+        address indexed _creator,
+        bytes32 indexed _id,
+        address _token,
+        bytes32[] _data,
+        uint256 _salt
+    );
+
+    event Created3(
+        address indexed _creator,
+        bytes32 indexed _id,
+        address _token,
+        bytes32[] _data,
+        uint256 _salt
+    );
+
+    event Played(
+        address indexed _sender,
+        address indexed _player,
+        bytes32 indexed _id,
+        uint256 _amount,
+        bytes32[] _data
+    );
+
+    event Collected(
+        address indexed _collecter,
+        bytes32 indexed _id,
+        address indexed _beneficiary,
+        uint256 _amount, 
+        bytes32[] _data
+    );
+
+    event Canceled(
+        address indexed _creator,
+        bytes32 indexed _id,
+        uint256 _amount,
+        bytes32[] _data
+    );
 }
