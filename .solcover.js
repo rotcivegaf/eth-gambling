51b85c399cb1dd7280304f3338b16a144660d4ad
rotcivegaf <victorfage@gmail.com>
2019-01-29 03:06:57 -0300

    skip tests files

--- .solcover.js
+++ .solcover.js
@@ -3,6 +3,6 @@ module.exports = {
     testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
     compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
     skipFiles: [ // TODO: Remove skip BytesUtils when solidity-coverage gets patched
-        'utils'
+        'utils/test'
     ]
 }
