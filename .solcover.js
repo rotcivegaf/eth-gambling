519bc35b2fa3570e4b24ea1f6c94b9c7310ec506
rotcivegaf <victorfage@gmail.com>
2019-04-17 00:01:41 -0300

    change to 2 spaces

--- .solcover.js
+++ .solcover.js
@@ -1,10 +1,10 @@
 module.exports = {
-    norpc: true,
-    testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
-    compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
-    skipFiles: [
-        'utils/test',
-        'utils/BytesUtils.sol',
-        'interfaces'
-    ]
+  norpc: true,
+  testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
+  compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
+  skipFiles: [
+    'utils/test',
+    'utils/BytesUtils.sol',
+    'interfaces'
+  ]
 }
