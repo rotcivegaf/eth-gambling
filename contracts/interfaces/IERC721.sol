c0bd08d000bf51d9f020a6aa6f22f8d59267756f
rotcivegaf <victorfage@gmail.com>
2019-07-17 01:32:56 -0300

    update event of erc721

--- contracts/interfaces/IERC721.sol
+++ contracts/interfaces/IERC721.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.5.6;
 
 
 interface IERC721 {
-    event Transfer(address indexed _from, address indexed _to, uint256 _tokenId);
-    event Approval(address indexed _owner, address indexed _approved, uint256 _tokenId);
+    event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
+    event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
     event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
 
     function balanceOf(address _owner) external view returns (uint256);
