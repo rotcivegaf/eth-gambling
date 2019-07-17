6e8e1a3a6717490d2c33872de861822e99fa9d10
rotcivegaf <victorfage@gmail.com>
2019-07-17 01:32:39 -0300

    change event Tip erc721

--- contracts/interfaces/ITipERC721.sol
+++ contracts/interfaces/ITipERC721.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.6;
 
 
 contract ITipERC721 {
-    event Tip(uint256 _erc721Id);
+    event Tip(address indexed _from, address indexed _erc721, uint256 _erc721Id);
 
     function tip(address _from, address _erc721, uint256 _erc721Id) external;
 }
