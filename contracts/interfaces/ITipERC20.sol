bf71b5dd9b6834acb2f6a08971df8cca3b7fbf5e
rotcivegaf <victorfage@gmail.com>
2019-07-22 00:18:30 -0300

    change to interface

--- contracts/interfaces/ITipERC20.sol
+++ contracts/interfaces/ITipERC20.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 
 
-contract ITipERC20 {
+interface ITipERC20 {
     event Tip(address indexed _from, address indexed _token, uint256 _amount);
 
     function tip(address _from, address _token, uint256 _amount) external payable;
--- contracts/interfaces/ITipERC721.sol
+++ contracts/interfaces/ITipERC721.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.6;
 
 
-contract ITipERC721 {
+interface ITipERC721 {
     event Tip(address indexed _from, address indexed _erc721, uint256 _erc721Id);
 
     function tip(address _from, address _erc721, uint256 _erc721Id) external;
